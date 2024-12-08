0aa3ec87b14d9f7608a7fac9dc6a2bbaccd4ded9
hari5ghb <ghub4868@gmil.com>
2024-12-08 09:41:05 +0530

    original

--- src/Components/Banner.jsx
+++ src/Components/Banner.jsx
@@ -12,7 +12,7 @@ const Banner = () => {
                         <h3 className='mt-16 text-start font-semibold font-poppins text-orange-500'>About D-MAS (Business Marketing Solution)</h3>
                         <p className='mt-5 text-start font-normal'>At D-MAS (Business Marketing Solution), we transform how businesses connect with their audiences by crafting tailored digital marketing strategies.</p>
                         <p className='mt-5 text-start font-normal'>By combining innovative approaches, data-driven insights, and cutting-edge tools, we ensure your brand resonates with the right audience and delivers measurable results.</p>
-                        <p className='mt-5 text-start font-normal'>Our focus on collaboration, transparency, and growth enables us to turn your business goals into achievements, empowering you to thrive in the ever - evolving digital landscape.</p>
+                        <p className='mt-5 text-start font-normal'>Our focus on collaboration, transparency, and growth enables us to turn your business goals into achievements, empowering you to thrive in the ever-evolving digital landscape.</p>
                     </div>
                     <div className='w-1/2 p-5 bg-white rounded-lg shadow-lg'>
                         <img src={banner} width={600} height={400} className='rounded-2xl ml-10'/>
